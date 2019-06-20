193303868e655670400d9f411a040df14b529838
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2019-06-20 18:55:10 +0100

    some finishing changes

--- Lloyds-of-london-taking-control-page-clone/js/script.js
+++ Lloyds-of-london-taking-control-page-clone/js/script.js
@@ -1,10 +1,21 @@
-var service = document.getElementById("drop-down");
-var subMenu = document.getElementById("sub-menu");
-var a = document.querySelectorAll(".secondary-item");
-console.log(a);
-service.addEventListener('mouseover', function () {
+let dropDown = document.getElementById("drop-down");
+let subMenu = document.getElementById("sub-menu");
+let a = document.querySelectorAll(".secondary-item");
+let share = document.getElementById("share");
+let socialMedia = document.getElementById("smedia")
+
+
+dropDown.addEventListener('mouseover', function () {
 	subMenu.style.display = "block";
 })
-service.addEventListener('mouseout', function () {
+dropDown.addEventListener('mouseout', function () {
 	subMenu.style.display = "none";
+})
+socialMedia.style.display = "none";
+
+share.addEventListener("click", function(){
+	socialMedia.style.display = "";
+	share.classList.toggle("bg-black");
+	socialMedia.classList.toggle("smedia");
+	socialMedia.classList.toggle("bg-black");
 })
